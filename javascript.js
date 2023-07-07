66617735d6a1c56dc037cb9393abc21bf95f9cb4
Wojciech Przyluski <przyluskiwojciech@gmail.com>
2023-07-07 13:27:13 +0200

    Final animation for section SuperPowers

--- javascript.js
+++ javascript.js
@@ -34,11 +34,62 @@ logoSwitch('.section-3-logo', '.section-2-logo', '.section-1-logo');
 logoSwitch('.section-4-logo', '.section-3-logo', '.section-2-logo');
 logoSwitch('.section-5-logo', '.section-4-logo', '.section-1-logo');
 
-// Superpowers & Starring Image Animation
+
+// Superpowers Image Animation for Small Screens
 
 window.addEventListener('scroll', function ()
 {
-    var images = document.querySelectorAll('.brand-experience, .brand-partnership, .branded-content, .creative-medium, #natalia-image, #mateusz-image, #bartosz-image');
+    var mediaQuerySmall = window.matchMedia('(max-width: 767.99px)');
+
+    if (mediaQuerySmall.matches)
+    {
+        var images = document.querySelectorAll('.brand-experience, .brand-partnership, .branded-content, .creative-medium');
+        var windowHeight = window.innerHeight;
+
+        for (var i = 0; i < images.length; i++)
+        {
+            var image = images[i];
+            var rect = image.getBoundingClientRect();
+
+            if (rect.bottom <= windowHeight / 2 && rect.bottom >= windowHeight * 0.15)
+            {
+                image.classList.add('expanded');
+                image.style.transform = 'scale(1.1)';
+            } else
+            {
+                image.classList.remove('expanded');
+                image.style.transform = 'scale(1)';
+            }
+        }
+    }
+});
+
+// Superpowers Image Animation for Medium and Larger Screens
+
+var mediumImages = document.querySelectorAll('.brand-experience, .brand-partnership, .branded-content, .creative-medium');
+
+for (var i = 0; i < mediumImages.length; i++)
+{
+    var image = mediumImages[i];
+
+    image.addEventListener('mouseenter', function ()
+    {
+        this.classList.add('expanded');
+        this.style.transform = 'scale(1.1)';
+    });
+
+    image.addEventListener('mouseleave', function ()
+    {
+        this.classList.remove('expanded');
+        this.style.transform = 'scale(1)';
+    });
+}
+
+// Starring Image Animation
+
+window.addEventListener('scroll', function ()
+{
+    var images = document.querySelectorAll('#natalia-image, #mateusz-image, #bartosz-image');
     var windowHeight = window.innerHeight;
 
     for (var i = 0; i < images.length; i++)
