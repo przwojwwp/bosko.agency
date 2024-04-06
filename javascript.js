39a056f65a8967781f69f721933b5e2299976996
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-04-06 20:08:24 +0200

    Back to the first version

--- javascript.js
+++ javascript.js
@@ -3,16 +3,16 @@
 
 // Detect request animation frame
 var scroll =
-    window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.msRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    // IE Fallback, you can even fallback to onscroll
-    function (callback)
-    {
-        window.setTimeout(callback, 1000 / 60);
-    };
+  window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.msRequestAnimationFrame ||
+  window.oRequestAnimationFrame ||
+  // IE Fallback, you can even fallback to onscroll
+  function (callback)
+  {
+    window.setTimeout(callback, 1000 / 60);
+  };
 var lastPosition = -1;
 
 // my Variables
@@ -24,95 +24,95 @@ var replaceItemHeight = -1;
 // The Scroll Function
 function loop()
 {
-    var top = window.pageYOffset;
-    // my variables
+  var top = window.pageYOffset;
+  // my variables
 
-    // my sections to calculate stuff
-    var sections = document.querySelectorAll(".section");
-    var replaceContainer = document.querySelectorAll(".js-replace");
-    var replaceItem = document.querySelectorAll(".js-replace__item");
+  // my sections to calculate stuff
+  var sections = document.querySelectorAll(".section");
+  var replaceContainer = document.querySelectorAll(".js-replace");
+  var replaceItem = document.querySelectorAll(".js-replace__item");
 
-    if (replaceItem.length > 0)
-    {
-        // get top position of item from container, because image might not have loaded
-        replaceItemTop = parseInt(
-            replaceContainer[0].getBoundingClientRect().top
-        );
-        replaceItemHeight = replaceItem[0].offsetHeight;
-        replaceItemBottom = replaceItemTop + replaceItemHeight;
-    }
+  if (replaceItem.length > 0)
+  {
+    // get top position of item from container, because image might not have loaded
+    replaceItemTop = parseInt(
+      replaceContainer[0].getBoundingClientRect().top
+    );
+    replaceItemHeight = replaceItem[0].offsetHeight;
+    replaceItemBottom = replaceItemTop + replaceItemHeight;
+  }
 
-    var sectionTop = -1;
-    var sectionBottom = -1;
-    var currentSection = -1;
+  var sectionTop = -1;
+  var sectionBottom = -1;
+  var currentSection = -1;
 
-    // Fire when needed
-    if (lastPosition == window.pageYOffset)
-    {
-        scroll(loop);
-        return false;
-    } else
-    {
-        lastPosition = window.pageYOffset;
-
-        // Your Function
-        Array.prototype.forEach.call(sections, function (el, i)
-        {
-            sectionTop = parseInt(el.getBoundingClientRect().top);
-            sectionBottom = parseInt(el.getBoundingClientRect().bottom);
-
-            // active section
-            if (
-                sectionTop <= replaceItemBottom &&
-                sectionBottom > replaceItemTop
-            )
-            {
-                // check if current section has bg
-                currentSection = el.classList.contains("section--bg");
-
-                // switch class depending on background image
-                if (currentSection)
-                {
-                    replaceContainer[0].classList.remove("js-replace--reverse");
-                } else
-                {
-                    replaceContainer[0].classList.add("js-replace--reverse");
-                }
-            }
-            // end active section
-
-            // if active Section hits replace area
-            if (replaceItemTop < sectionTop && sectionTop <= replaceItemBottom)
-            {
-                // animate only, if section background changed
-                if (currentSection != lastSection)
-                {
-                    document.documentElement.style.setProperty(
-                        "--replace-offset",
-                        (100 / replaceItemHeight) *
-                        parseInt(sectionTop - replaceItemTop) +
-                        "%"
-                    );
-                }
-            }
-            // end active section in replace area
-
-            // if section above replace area
-            if (replaceItemTop >= sectionTop)
-            {
-                // set offset to 0 if you scroll too fast
-                document.documentElement.style.setProperty(
-                    "--replace-offset",
-                    0 + "%"
-                );
-                // set last section to current section
-                lastSection = currentSection;
-            }
-        });
-    }
-
-    // Recall the loop
+  // Fire when needed
+  if (lastPosition == window.pageYOffset)
+  {
     scroll(loop);
+    return false;
+  } else
+  {
+    lastPosition = window.pageYOffset;
+
+    // Your Function
+    Array.prototype.forEach.call(sections, function (el, i)
+    {
+      sectionTop = parseInt(el.getBoundingClientRect().top);
+      sectionBottom = parseInt(el.getBoundingClientRect().bottom);
+
+      // active section
+      if (
+        sectionTop <= replaceItemBottom &&
+        sectionBottom > replaceItemTop
+      )
+      {
+        // check if current section has bg
+        currentSection = el.classList.contains("section--bg");
+
+        // switch class depending on background image
+        if (currentSection)
+        {
+          replaceContainer[0].classList.remove("js-replace--reverse");
+        } else
+        {
+          replaceContainer[0].classList.add("js-replace--reverse");
+        }
+      }
+      // end active section
+
+      // if active Section hits replace area
+      if (replaceItemTop < sectionTop && sectionTop <= replaceItemBottom)
+      {
+        // animate only, if section background changed
+        if (currentSection != lastSection)
+        {
+          document.documentElement.style.setProperty(
+            "--replace-offset",
+            (100 / replaceItemHeight) *
+            parseInt(sectionTop - replaceItemTop) +
+            "%"
+          );
+        }
+      }
+      // end active section in replace area
+
+      // if section above replace area
+      if (replaceItemTop >= sectionTop)
+      {
+        // set offset to 0 if you scroll too fast
+        document.documentElement.style.setProperty(
+          "--replace-offset",
+          0 + "%"
+        );
+        // set last section to current section
+        lastSection = currentSection;
+      }
+    });
+  }
+
+  // Recall the loop
+  scroll(loop);
 }
 
 // Call the loop for the first time
@@ -120,51 +120,51 @@ loop();
 
 window.onresize = function (event)
 {
-    loop();
+  loop();
 };
 
 // Function to adjust the font size based on the current width and height
 function adjustFontSize(width, height)
 {
-    const textContent = document.querySelector(".section-1-text-content");
-    const maxWidth = 96; // Maximum width at which the font size remains 7vw
-    const maxHeight = 96; // Maximum height at which the font size remains 7vw
-    const maxFontSize = 7; // The maximum font size in vw
+  const textContent = document.querySelector(".section-1-text-content");
+  const maxWidth = 96; // Maximum width at which the font size remains 7vw
+  const maxHeight = 96; // Maximum height at which the font size remains 7vw
+  const maxFontSize = 7; // The maximum font size in vw
 
-    // Calculate the font size for width and height separately
-    const widthFontSize = (width / maxWidth) * maxFontSize;
-    const heightFontSize = (height / maxHeight) * maxFontSize;
+  // Calculate the font size for width and height separately
+  const widthFontSize = (width / maxWidth) * maxFontSize;
+  const heightFontSize = (height / maxHeight) * maxFontSize;
 
-    // Use the smaller font size to maintain the aspect ratio of the text
-    const fontSize = Math.min(widthFontSize, heightFontSize);
+  // Use the smaller font size to maintain the aspect ratio of the text
+  const fontSize = Math.min(widthFontSize, heightFontSize);
 
-    // Apply the font size to the text content
-    textContent.style.fontSize = fontSize + "vw";
+  // Apply the font size to the text content
+  textContent.style.fontSize = fontSize + "vw";
 }
 
 // Function to adjust the position of the text content
 function adjustTextPosition()
 {
-    const textContent = document.querySelector(".section-1-text-content");
-    const section1 = document.querySelector(".section-1");
-    const initialTop = 48; // Initial top position in percentage (50%)
-    const finalTop = 30; // Final top position in percentage (32%)
+  const textContent = document.querySelector(".section-1-text-content");
+  const section1 = document.querySelector(".section-1");
+  const initialTop = 48; // Initial top position in percentage (50%)
+  const finalTop = 30; // Final top position in percentage (32%)
 
-    // Calculate the interpolation factor based on the width and height
-    const interpolationFactor = (initialWidth - width) / (initialWidth - 64);
-    const topPosition = initialTop - interpolationFactor * (initialTop - finalTop);
+  // Calculate the interpolation factor based on the width and height
+  const interpolationFactor = (initialWidth - width) / (initialWidth - 64);
+  const topPosition = initialTop - interpolationFactor * (initialTop - finalTop);
 
-    // Apply the new top position using CSS top property
-    textContent.style.top = topPosition + "%";
+  // Apply the new top position using CSS top property
+  textContent.style.top = topPosition + "%";
 }
 
 // Function to adjust the font size and update the width and height
 function updateSize()
 {
-    section1.style.width = width + "vw";
-    section1.style.height = height + "vh";
-    adjustFontSize(width, height);
-    adjustTextPosition();
+  section1.style.width = width + "vw";
+  section1.style.height = height + "vh";
+  adjustFontSize(width, height);
+  adjustTextPosition();
 }
 
 // Global variable to track content section change
@@ -186,66 +186,66 @@ let isSectionChanged = false;
 // Function to toggle the "overflow: hidden" class on the body based on pageHolder value
 function toggleBodyOverflow()
 {
-    document.body.style.overflow = pageHolder ? "hidden" : "auto";
+  document.body.style.overflow = pageHolder ? "hidden" : "auto";
 }
 
 // Function to set pageHolder and toggle body overflow
 function setPageHolder(value, delay)
 {
-    if (timeoutId) clearTimeout(timeoutId); // Clear previous timeout, if any
+  if (timeoutId) clearTimeout(timeoutId); // Clear previous timeout, if any
 
-    timeoutId = setTimeout(function ()
-    {
-        pageHolder = value;
-        toggleBodyOverflow(); // Toggle body overflow based on the new pageHolder value
-    }, delay);
+  timeoutId = setTimeout(function ()
+  {
+    pageHolder = value;
+    toggleBodyOverflow(); // Toggle body overflow based on the new pageHolder value
+  }, delay);
 }
 
 // Function to adjust the font size and update the width and height
 function updateSize()
 {
-    section1.style.width = width + "vw";
-    section1.style.height = height + "vh";
-    adjustFontSize(width, height);
-    adjustTextPosition();
+  section1.style.width = width + "vw";
+  section1.style.height = height + "vh";
+  adjustFontSize(width, height);
+  adjustTextPosition();
 }
 
 // Obsługa zdarzenia scroll na przycisku myszy
 containerFluid.addEventListener("wheel", function (event)
 {
-    if (event.deltaY < 0)
+  if (event.deltaY < 0)
+  {
+    // Scroll w górę
+    if (scrollY === 0 && width < initialWidth && height < initialHeight)
     {
-        // Scroll w górę
-        if (scrollY === 0 && width < initialWidth && height < initialHeight)
-        {
-            width += 1;
-            height += 1;
-        }
-    } else
-    {
-        // Scroll w dół
-        if (width > 64 && height > 60)
-        {
-            width -= 1;
-            height -= 1;
-        }
+      width += 1;
+      height += 1;
     }
-
-    // Check if the text content should change
-    if (width <= 64 && height <= 60 && !isSectionChanged)
+  } else
+  {
+    // Scroll w dół
+    if (width > 64 && height > 60)
     {
-        section1.innerHTML = `
+      width -= 1;
+      height -= 1;
+    }
+  }
+
+  // Check if the text content should change
+  if (width <= 64 && height <= 60 && !isSectionChanged)
+  {
+    section1.innerHTML = `
         <div class="video-container">
-          <video src="video/BOSKO_REEL_v4.mp4" controls poster="img/poster.png"></video>
+          <video src="video/BOSKO_REEL_v4.mp4" controls poster="img/boskoposter.png"></video>
         </div>
       `;
-        isSectionChanged = true;
-        setPageHolder(false, 300); // Set pageHolder to false after a 1-second delay
-    }
+    isSectionChanged = true;
+    setPageHolder(false, 300); // Set pageHolder to false after a 1-second delay
+  }
 
-    if (width >= 96 && height >= 96 && sectionResize && isSectionChanged)
-    {
-        section1.innerHTML = `
+  if (width >= 96 && height >= 96 && sectionResize && isSectionChanged)
+  {
+    section1.innerHTML = `
         <div class="section-1-video-wrapper">
           <div class="section-1-curtain"></div>
           <video src="video/BOSKO_REEL_v4.mp4" autoplay muted loop></video>
@@ -274,12 +274,12 @@ containerFluid.addEventListener("wheel", function (event)
           </div>
         </div>
       `;
-        setPageHolder(true, 1); // Set pageHolder to true after a 0.001-second delay
-        isSectionChanged = false;
-    }
+    setPageHolder(true, 1); // Set pageHolder to true after a 0.001-second delay
+    isSectionChanged = false;
+  }
 
-    // Update the size and font size
-    updateSize();
+  // Update the size and font size
+  updateSize();
 });
 
 // Call the updateSize function initially to set the initial size and position
@@ -289,54 +289,54 @@ updateSize();
 
 if (window.matchMedia('(max-width: 767.99px)').matches)
 {
-    window.addEventListener('scroll', function ()
+  window.addEventListener('scroll', function ()
+  {
+    var images = document.querySelectorAll('.brand-experience, .brand-partnership, .branded-content, .creative-medium');
+    var windowHeight = window.innerHeight;
+
+    for (var i = 0; i < images.length; i++)
     {
-        var images = document.querySelectorAll('.brand-experience, .brand-partnership, .branded-content, .creative-medium');
-        var windowHeight = window.innerHeight;
+      var image = images[i];
+      var rect = image.getBoundingClientRect();
 
-        for (var i = 0; i < images.length; i++)
-        {
-            var image = images[i];
-            var rect = image.getBoundingClientRect();
-
-            if (rect.bottom <= windowHeight / 2 && rect.bottom >= windowHeight * 0.15)
-            {
-                image.classList.add('expanded');
-                image.style.transform = 'scale(1.1)';
-            } else
-            {
-                image.classList.remove('expanded');
-                image.style.transform = 'scale(1)';
-            }
-        }
-    });
+      if (rect.bottom <= windowHeight / 2 && rect.bottom >= windowHeight * 0.15)
+      {
+        image.classList.add('expanded');
+        image.style.transform = 'scale(1.1)';
+      } else
+      {
+        image.classList.remove('expanded');
+        image.style.transform = 'scale(1)';
+      }
+    }
+  });
 }
 
 // Superpowers Image Animation for Small Screens
 
 if (window.matchMedia('(max-width: 767.99px)').matches)
 {
-    window.addEventListener('scroll', function ()
+  window.addEventListener('scroll', function ()
+  {
+    var images = document.querySelectorAll('.brand-experience, .brand-partnership, .branded-content, .creative-medium');
+    var windowHeight = window.innerHeight;
+
+    for (var i = 0; i < images.length; i++)
     {
-        var images = document.querySelectorAll('.brand-experience, .brand-partnership, .branded-content, .creative-medium');
-        var windowHeight = window.innerHeight;
+      var image = images[i];
+      var rect = image.getBoundingClientRect();
 
-        for (var i = 0; i < images.length; i++)
-        {
-            var image = images[i];
-            var rect = image.getBoundingClientRect();
-
-            if (rect.bottom <= windowHeight / 2 && rect.bottom >= windowHeight * 0.15)
-            {
-                image.classList.add('expanded');
-                image.style.transform = 'scale(1.1)';
-            } else
-            {
-                image.classList.remove('expanded');
-                image.style.transform = 'scale(1)';
-            }
-        }
-    });
+      if (rect.bottom <= windowHeight / 2 && rect.bottom >= windowHeight * 0.15)
+      {
+        image.classList.add('expanded');
+        image.style.transform = 'scale(1.1)';
+      } else
+      {
+        image.classList.remove('expanded');
+        image.style.transform = 'scale(1)';
+      }
+    }
+  });
 }
 
 // Superpowers Image Animation for Medium and Larger Screens
@@ -345,19 +345,19 @@ var mediumImages = document.querySelectorAll('.brand-experience, .brand-partners
 
 for (var i = 0; i < mediumImages.length; i++)
 {
-    var image = mediumImages[i];
+  var image = mediumImages[i];
 
-    image.addEventListener('mouseenter', function ()
-    {
-        this.classList.add('expanded');
-        this.style.transform = 'scale(1.1)';
-    });
+  image.addEventListener('mouseenter', function ()
+  {
+    this.classList.add('expanded');
+    this.style.transform = 'scale(1.1)';
+  });
 
-    image.addEventListener('mouseleave', function ()
-    {
-        this.classList.remove('expanded');
-        this.style.transform = 'scale(1)';
-    });
+  image.addEventListener('mouseleave', function ()
+  {
+    this.classList.remove('expanded');
+    this.style.transform = 'scale(1)';
+  });
 }
 
 // Starring Image Animation
@@ -367,31 +367,31 @@ const images = document.querySelectorAll('.section-4-img-container .img-fluid');
 // Ustawienie początkowego stanu obrazów na niewidoczne
 images.forEach(image =>
 {
-    image.style.opacity = '0';
+  image.style.opacity = '0';
 });
 
 // Funkcja do obsługi efektu pojawiania się obrazów
 function fadeInImages()
 {
-    // Sprawdzanie, czy obraz jest w widoku przeglądarki
-    images.forEach((image, index) =>
+  // Sprawdzanie, czy obraz jest w widoku przeglądarki
+  images.forEach((image, index) =>
+  {
+    const rect = image.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    // Jeśli obraz jest w widoku przeglądarki
+    if (rect.top <= windowHeight)
     {
-        const rect = image.getBoundingClientRect();
-        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+      // Opóźnienie pojawiania się obrazu o 0.5 sekundy * indeks obrazu
+      const delay = 500 * index;
 
-        // Jeśli obraz jest w widoku przeglądarki
-        if (rect.top <= windowHeight)
-        {
-            // Opóźnienie pojawiania się obrazu o 0.5 sekundy * indeks obrazu
-            const delay = 500 * index;
-
-            // Ustawienie animacji na obrazie po opóźnieniu
-            setTimeout(() =>
-            {
-                image.style.animation = 'fadeIn 1s forwards ease';
-            }, delay);
-        }
-    });
+      // Ustawienie animacji na obrazie po opóźnieniu
+      setTimeout(() =>
+      {
+        image.style.animation = 'fadeIn 1s forwards ease';
+      }, delay);
+    }
+  });
 }
 
 // Obsługa pojawiania się obrazów przy załadowaniu strony
@@ -405,8 +405,8 @@ window.addEventListener('scroll', fadeInImages);
 
 function topFunction()
 {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 }
